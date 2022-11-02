d4b798865de520454dad25607029c6bb64a09172
Aram Panasenco <panasenco@users.noreply.github.com>
2022-11-02 04:43:57 +0000

    Now installing dbt and running dbt docs

--- postcreate.bash
+++ postcreate.bash
@@ -8,4 +8,12 @@ if [[ ! -d demo ]]; then
     meltano config target-postgres set user postgres
     meltano config target-postgres set password postgres
     meltano config target-postgres set dbname postgres
+    meltano add transformer dbt-postgres
+    meltano config dbt-postgres set host warehouse
+    meltano config dbt-postgres set port 5432
+    meltano config dbt-postgres set user postgres
+    meltano config dbt-postgres set password postgres
+    meltano config dbt-postgres set dbname postgres
+    meltano config dbt-postgres set schema analytics
+    meltano invoke dbt-postgres:docs-generate
 fi
